62dd8bac4d1b87c813acb6e3729d7e8e35300ed4
Sushan-Shrestha-github <sushanshrestha61@gmail.com>
2026-06-12 21:12:37 +0530

    First Commit

---
+++ Array/prod.java
@@ -0,0 +1,11 @@
+class prod{
+    public static void main(String args[]){
+        int arr = {2, 3, 4};
+        int n = arr.length;
+        int prod = 1;
+        for(int i = 0; i<n; i++){
+            prod = prod*arr[i];
+        }
+        System.out.println(prod);
+    }
+}
